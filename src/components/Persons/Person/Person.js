7bcbb1bf84be7873b2dc57eb94084127b20f9cc6
PEREZ, EDGAR <andres.perez@LibertyMutual.com>
2020-06-05 09:29:01 -0500

    Adds Refs

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types'
 
 import classes from './Person.module.css'
@@ -6,11 +6,18 @@ import Aux from '../../../hoc/Aux'
 import withLogic from '../../../hoc/withLogic'
 
 const Person = (props) => {
+    const inputElRef = useRef(null);
+    useEffect(() => inputElRef.current.focus());
     return (
         <Aux>
             <p onClick={props.click} > I 'm {props.name} and I am {props.age} years old!</p>
             <p> {props.children} </p>
-            <input type="text" onChange={props.changed} value={props.name} />
+            <input
+                ref={inputElRef}
+                type="text"
+                onChange={props.changed}
+                value={props.name}
+            />
         </Aux>
     )
 }
